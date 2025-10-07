0c4019297c08b4a46cd735ede3c5da5ceb914ed1
mahmoud rabea mahmoud moatasem <rabea01mahmoud@gmail.com>
2025-10-07 05:15:58 +0300

    Update # Rolling a dice.py

--- # Rolling a dice.py
+++ # Rolling a dice.py
@@ -1,13 +1,16 @@
 # Rolling a dice
 
+##============Setup==============##
 # NumPy is imported, seed is set
 import numpy as np
 # Starting step
 step = 50
 
+##============Roll the dice=============##
 # Roll the dice
 dice = np.random.randint(1, 7)
 
+##===========Movement rules============##
 # Finish the control construct
 if dice <= 2:
     step = step - 1
@@ -16,6 +19,7 @@ elif dice <= 5:
 else:
     step = step + np.random.randint(1, 7)
 
+##==============Print results===============##
 # Print out dice and step
 print(dice)
 print(step)
